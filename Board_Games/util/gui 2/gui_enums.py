e01fad020ecb6e7e9754273e34f46aab889c9ec5
Chris Thomas <175346923+mmccthomas@users.noreply.github.com>
2025-04-13 12:45:36 +0100

    clear some unused modules

--- Board_Games/util/gui 2/gui_enums.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    PLAYER_1 = 'o'
-    PLAYER_2 = '@'
-    EMPTY = '.'
-    PLAYERS =[PLAYER_1, PLAYER_2]
-    PIECES = ['emj:Red_Circle', 'emj:Moon_5']
-    # PIECE_NAMES ={RED: 'Red', YELLOW: 'Yellow'}
