980843fce3e8252b128298fcf069751e04f82570
Chris Thomas <175346923+mmccthomas@users.noreply.github.com>
2025-04-24 21:53:29 +0100

    suguru calculation and solver test

--- Word_Games/test_suguru.py
+++ Word_Games/test_suguru.py
@@ -7,12 +7,12 @@ from time import sleep, time
 import numpy as np
 import gc
 obj = None
-names = ['Easy', 'Guardian', 'Medium', 'Hard', 'Hardest']
+names = ['Easy', 'Regular', 'Medium'] #, 'Hard'] # 'Hardest']
 
 obj = suguru.Suguru()   
 obj.debug = False
 for name in reversed(names): 
-    for i in range(3):    
+    for i in range(5):    
         obj.test = name
         print(f'{name=}, #{i}')
         #obj.initialise_board()
